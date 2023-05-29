44f9abbddc9dfbdb7256b824f4ffa912a6b0bd28
Max Axanii <maxaxanii@Maxs-MacBook-Air.local>
2023-05-29 23:04:13 +0300

    Updated how comments are displayed

--- src/Components/DataFetch/PostDataComponent.tsx
+++ src/Components/DataFetch/PostDataComponent.tsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 interface Post {
   id: number;
   title: string;
 }
 
-interface PostDataFetchProps {
-  isClicked: boolean;
-  posts: Post[];
+interface Comment {
+  id: number;
+  postId: number;
+  name: string;
+  email: string;
+  body: string;
 }
 
-const PostDataFetch: React.FC<PostDataFetchProps> = ({ isClicked, posts }) => {
+const PostDataFetch: React.FC<{ posts: Post[] }> = ({ posts }) => {
+  const [displayedCommentIds, setDisplayedCommentIds] = useState<number[]>([]);
+  const [comments, setComments] = useState<{ [postId: number]: Comment[] }>({});
+
+  const fetchComments = async (postId: number) => {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+      const fetchedComments: Comment[] = response.data;
+      return fetchedComments;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const handleDisplayComment = async (postId: number) => {
+    if (displayedCommentIds.includes(postId)) {
+      setDisplayedCommentIds(prevIds => prevIds.filter(id => id !== postId));
+    } else {
+      setDisplayedCommentIds(prevIds => [...prevIds, postId]);
+      if (!comments[postId]) {
+        const fetchedComments = await fetchComments(postId);
+        setComments(prevComments => ({
+          ...prevComments,
+          [postId]: fetchedComments,
+        }));
+      }
+    }
+  };
+
   return (
-    <>
-      {isClicked && (
-        <ul>
-          {posts.map((post: Post, index: number) => (
-            <div key={index} className="">
-              <li className="font-bold">{post.id} | {post.title} </li>
-              <p>{post.title}</p>
-            </div>
-          ))}
-        </ul>
-      )}
-    </>
+    <ul>
+      {posts.map((post: Post) => (
+        <div key={post.id} className="">
+          <li className="font-bold">{post.id} | {post.title}</li>
+          <p>{post.title}</p>
+          <button onClick={() => handleDisplayComment(post.id)} className="border-2 border-gray-500 rounded-2xl hover:bg-gray-700 bg-gray-500 text-slate-50 px-2 my-1">
+            Display Comments
+          </button>
+          {displayedCommentIds.includes(post.id) && comments[post.id] && (
+            <ul>
+              {comments[post.id].map((comment: Comment) => (
+                <div key={comment.id} className="">
+                  <li className="font-bold">{comment.id} | {comment.name}</li>
+                  <p>{comment.body}</p>
+                </div>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </ul>
   );
 };
 
--- src/Components/PostDisplay.tsx
+++ src/Components/PostDisplay.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import ButtonDisplayItemsComponent from "./ButtonDisplayItemsComponent";
-import CommentDataFetch from "./DataFetch/CommentData";
 import PostDataFetch from "./DataFetch/PostDataComponent";
 
 interface Comment {
@@ -14,32 +13,15 @@ interface Comment {
 interface Post {
   id: number;
   title: string;
+  body: string;
 }
 
 const PostContainer: React.FC = () => {
-  const [isCommentClicked, setIsCommentClicked] = useState(false);
   const [isPostClicked, setIsPostClicked] = useState(false);
-  const [commentData, setCommentData] = useState<Comment[]>([]);
   const [postData, setPostData] = useState<Post[]>([]);
-
-  const toggleCommentVisibility = () => {
-    setIsCommentClicked(prevState => !prevState);
-    setIsPostClicked(false);
-  };
 
   const togglePostVisibility = () => {
     setIsPostClicked(prevState => !prevState);
-    setIsCommentClicked(false);
-  };
-
-  const fetchComments = () => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/comments")
-      .then(res => {
-        const commentData: Comment[] = res.data.slice(0, 10);
-        setCommentData(commentData);
-      })
-      .catch(err => console.log(err));
   };
 
   const fetchPosts = () => {
@@ -51,16 +33,6 @@ const PostContainer: React.FC = () => {
       })
       .catch(err => console.log(err));
   };
-
-  const renderCommentComponent = () => {
-    if (isCommentClicked) {
-      if (commentData.length === 0) {
-        fetchComments();
-      }
-      return <CommentDataFetch comments={commentData} isClicked={isCommentClicked} />;
-    }
-    return null;
-  };
   
 
   const renderPostComponent = () => {
@@ -68,28 +40,19 @@ const PostContainer: React.FC = () => {
       if (postData.length === 0) {
         fetchPosts();
       }
-      return <PostDataFetch posts={postData} isClicked={isPostClicked} />;
+      return <PostDataFetch posts={postData}/>;
     }
     return null;
   };
 
   return (
     <div className="">
-      <div className="">
-        <ButtonDisplayItemsComponent
-          name="Comment"
-          onClick={toggleCommentVisibility}
-        />
         <ButtonDisplayItemsComponent
           name="Post"
           onClick={togglePostVisibility}
         />
-      </div>
-      <div className="">
-        {renderCommentComponent()}
         {renderPostComponent()}
       </div>
-    </div>
   );
 };
 
